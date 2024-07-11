cede38381e267f8b57102d56991e0303db1df011
chenlt-qtl <da.mu@163.com>
2024-07-11 18:25:33 +0800

    单词发音

--- ui/src/views/eng/word/index.vue
+++ ui/src/views/eng/word/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form
+      :model="queryParams"
+      ref="queryForm"
+      size="small"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
       <el-form-item label="单词内容" prop="wordName">
         <el-input
           v-model="queryParams.wordName"
@@ -18,8 +25,16 @@
         />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-search"
+          size="mini"
+          @click="handleQuery"
+          >搜索</el-button
+        >
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery"
+          >重置</el-button
+        >
       </el-form-item>
     </el-form>
 
@@ -32,7 +47,8 @@
           size="mini"
           @click="handleAdd"
           v-hasPermi="['eng:word:add']"
-        >新增</el-button>
+          >新增</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -43,7 +59,8 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['eng:word:edit']"
-        >修改</el-button>
+          >修改</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -54,7 +71,8 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['eng:word:remove']"
-        >删除</el-button>
+          >删除</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -64,20 +82,38 @@
           size="mini"
           @click="handleExport"
           v-hasPermi="['eng:word:export']"
-        >导出</el-button>
+          >导出</el-button
+        >
       </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar
+        :showSearch.sync="showSearch"
+        @queryTable="getList"
+      ></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="wordList" @selection-change="handleSelectionChange">
+    <el-table
+      v-loading="loading"
+      :data="wordList"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="${comment}" align="center" prop="id" />
       <el-table-column label="单词内容" align="center" prop="wordName" />
       <el-table-column label="音标" align="center" prop="phAm" />
       <el-table-column label="解释" align="center" prop="acceptation" />
       <el-table-column label="手动注释" align="center" prop="exchange" />
-      <el-table-column label="音频位置" align="center" prop="phAnMp3" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <el-table-column label="音频" align="center" prop="phAnMp3">
+        <template v-if="scope.row.phAnMp3" slot-scope="scope">
+          <el-button type="text" @click="() => play(scope.row.phAnMp3)">
+            <svg-icon icon-class="sound" />
+          </el-button>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="操作"
+        align="center"
+        class-name="small-padding fixed-width"
+      >
         <template slot-scope="scope">
           <el-button
             size="mini"
@@ -85,20 +121,22 @@
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['eng:word:edit']"
-          >修改</el-button>
+            >修改</el-button
+          >
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['eng:word:remove']"
-          >删除</el-button>
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
-    
+
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       :page.sync="queryParams.pageNum"
       :limit.sync="queryParams.pageSize"
@@ -115,13 +153,17 @@
           <el-input v-model="form.phAm" placeholder="请输入音标" />
         </el-form-item>
         <el-form-item label="解释" prop="acceptation">
-          <el-input v-model="form.acceptation" type="textarea" placeholder="请输入内容" />
+          <el-input
+            v-model="form.acceptation"
+            type="textarea"
+            placeholder="请输入内容"
+          />
         </el-form-item>
         <el-form-item label="手动注释" prop="exchange">
           <el-input v-model="form.exchange" placeholder="请输入手动注释" />
         </el-form-item>
         <el-form-item label="音频位置" prop="phAnMp3">
-          <file-upload v-model="form.phAnMp3"/>
+          <file-upload v-model="form.phAnMp3" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -133,7 +175,14 @@
 </template>
 
 <script>
-import { listWord, getWordById, delWord, addWord, updateWord } from "@/api/eng/word";
+import {
+  listWord,
+  getWordById,
+  delWord,
+  addWord,
+  updateWord,
+} from "@/api/eng/word";
+import { play } from "@/utils/audio";
 
 export default {
   name: "Word",
@@ -170,19 +219,22 @@ export default {
       // 表单校验
       rules: {
         wordName: [
-          { required: true, message: "单词内容不能为空", trigger: "blur" }
+          { required: true, message: "单词内容不能为空", trigger: "blur" },
         ],
-      }
+      },
     };
   },
   created() {
     this.getList();
   },
   methods: {
+    play(url, mp3Time) {
+      play(url, mp3Time);
+    },
     /** 查询单词列表 */
     getList() {
       this.loading = true;
-      listWord(this.queryParams).then(response => {
+      listWord(this.queryParams).then((response) => {
         this.wordList = response.rows;
         this.total = response.total;
         this.loading = false;
@@ -207,7 +259,7 @@ export default {
         createTime: null,
         createBy: null,
         updateTime: null,
-        updateBy: null
+        updateBy: null,
       };
       this.resetForm("form");
     },
@@ -223,9 +275,9 @@ export default {
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.id)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
+      this.ids = selection.map((item) => item.id);
+      this.single = selection.length !== 1;
+      this.multiple = !selection.length;
     },
     /** 新增按钮操作 */
     handleAdd() {
@@ -236,8 +288,8 @@ export default {
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
-      const id = row.id || this.ids
-      getWordById(id).then(response => {
+      const id = row.id || this.ids;
+      getWordById(id).then((response) => {
         this.form = response.data;
         this.open = true;
         this.title = "修改单词";
@@ -245,16 +297,16 @@ export default {
     },
     /** 提交按钮 */
     submitForm() {
-      this.$refs["form"].validate(valid => {
+      this.$refs["form"].validate((valid) => {
         if (valid) {
           if (this.form.id != null) {
-            updateWord(this.form).then(response => {
+            updateWord(this.form).then((response) => {
               this.$modal.msgSuccess("修改成功");
               this.open = false;
               this.getList();
             });
           } else {
-            addWord(this.form).then(response => {
+            addWord(this.form).then((response) => {
               this.$modal.msgSuccess("新增成功");
               this.open = false;
               this.getList();
@@ -266,19 +318,27 @@ export default {
     /** 删除按钮操作 */
     handleDelete(row) {
       const ids = row.id || this.ids;
-      this.$modal.confirm('是否确认删除单词编号为"' + ids + '"的数据项？').then(function() {
-        return delWord(ids);
-      }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("删除成功");
-      }).catch(() => {});
+      this.$modal
+        .confirm('是否确认删除单词编号为"' + ids + '"的数据项？')
+        .then(function () {
+          return delWord(ids);
+        })
+        .then(() => {
+          this.getList();
+          this.$modal.msgSuccess("删除成功");
+        })
+        .catch(() => {});
     },
     /** 导出按钮操作 */
     handleExport() {
-      this.download('eng/word/export', {
-        ...this.queryParams
-      }, `word_${new Date().getTime()}.xlsx`)
-    }
-  }
+      this.download(
+        "eng/word/export",
+        {
+          ...this.queryParams,
+        },
+        `word_${new Date().getTime()}.xlsx`
+      );
+    },
+  },
 };
 </script>
