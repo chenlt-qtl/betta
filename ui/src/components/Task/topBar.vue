b656657c348a58758f41f6372ce593f727e12750
chenlt <da.mu@163.com>
2025-01-15 17:28:10 +0800

    缓存模块改进

--- ui/src/components/Task/topBar.vue
+++ ui/src/components/Task/topBar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
     <el-radio-group :value="taskStatus" size="mini" @input="changeStatus">
-      <el-radio-button label="0">所有</el-radio-button>
+      <el-radio-button label="0">All</el-radio-button>
       <el-radio-button label="1">进行中</el-radio-button>
       <el-radio-button label="2">已完成</el-radio-button>
     </el-radio-group>
     <el-radio-group :value="timeType" size="mini" @input="changeTimeType">
-      <el-radio-button label="0">所有</el-radio-button>
+      <el-radio-button label="0">All</el-radio-button>
       <el-radio-button label="1">本周</el-radio-button>
       <el-radio-button label="2">本月</el-radio-button>
     </el-radio-group>
