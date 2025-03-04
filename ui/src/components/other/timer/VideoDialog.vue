514fa92f7000c60f4423d2dd2b9006c5373eb2af
chenlt <da.mu@163.com>
2025-03-04 14:18:14 +0800

    加入视频点播

--- ui/src/components/other/timer/VideoDialog.vue
+++ ui/src/components/other/timer/VideoDialog.vue
@@ -1,24 +1,41 @@
 <template>
-    <el-dialog title="" :visible.sync="openWin" @close="onClose"  width="760px" append-to-body>
-        <video ref="videoPlayer" width="720" height="480">
+    <el-dialog title="" :visible.sync="openWin" @close="onClose" width="760px" append-to-body>
+        <el-button v-for="video, index in videos" :key="video.src" size="mini" :type="index == videoIdx ? '' : 'text'"
+            @click="() => playSelect(index - 1)">{{ video.name }}</el-button>
+        <video ref="videoPlayer" width="720" height="480" controls>
             <source :src="videoSrc" type="video/mp4">
             </source>
         </video>
     </el-dialog>
 </template>
 <script>
-
-const videos = ["lonely", "wangzi", "DaDaDa", "DiDiDiDiWa", "HotHotHot", "India", "mfsw", "TocaToca", "TheGreatest"]
-let videoIdx = 0;
-
-
 export default {
     props: ["open", "onClose"],
     data() {
         return {
             videoSrc: "",
-            openWin:this.open
+            openWin: this.open,
         };
+    },
+    created() {
+        this.videos = [{ name: "Lonely", src: "lonely" },
+        { name: "小丸子", src: "wangzi" },
+        { name: "Da Da Da", src: "DaDaDa" },
+        { name: "嘀嘀哇", src: "DiDiDiDiWa" },
+        { name: "Hot", src: "HotHotHot" },
+        { name: "印度歌", src: "India" },
+        { name: "眉飞色舞", src: "mfsw" },
+        { name: "Toca", src: "TocaToca" },
+        { name: "The Greatest", src: "TheGreatest" }];
+
+        this.videoIdx = -1;
+
+        this.$nextTick(() => {
+            this.$refs.videoPlayer.addEventListener("ended", this.playVideo);
+        })
+    },
+    beforeDestroy() {
+        this.$refs.videoPlayer.removeEventListener("ended", this.playVideo);
     },
     watch: {
         open() {
@@ -33,8 +50,9 @@ export default {
                 //关闭窗口
                 //关闭音乐
                 if (this.$refs.videoPlayer) {
-                    this.$refs.videoPlayer.removeEventListener("pause", this.playVideo);
-                    this.$refs.videoPlayer.pause();
+                    if (this.$refs.videoPlayer) {
+                        this.$refs.videoPlayer.pause();
+                    }
                 }
             }
         }
@@ -42,12 +60,17 @@ export default {
     },
     methods: {
         playVideo() {
-            this.videoSrc = process.env.VUE_APP_BASE_API + "/profile/sys/video/" + videos[videoIdx] + ".mp4";
-            if (++videoIdx >= videos.length) {
-                videoIdx = 0;
+            if (++this.videoIdx >= this.videos.length) {
+                this.videoIdx = 0;
             }
+            this.videoSrc = process.env.VUE_APP_BASE_API + "/profile/sys/video/" + this.videos[this.videoIdx].src + ".mp4";
             this.$refs.videoPlayer.load();
             this.$refs.videoPlayer.play();
+        },
+        //播放指定的音乐
+        playSelect(index) {
+            this.videoIdx = index;
+            this.playVideo();
         },
     },
 };
